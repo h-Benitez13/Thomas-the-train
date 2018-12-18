520f44f622bb8ca6712a00f514149c80bb871ba3
h-Benitez13 <hbenitez3711@gmail.com>
2018-12-18 05:51:51 -0500

    bootstrap to my liking, now focusing on the logic

---
+++ assets/javascript/train.js
@@ -0,0 +1,57 @@
+// firebase code to be asynchronous while initializing the page
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAHVV51cc_a8Wjyyo_MVxHUrRMRjE4bOns",
+    authDomain: "thomas-the-train-e852a.firebaseapp.com",
+    databaseURL: "https://thomas-the-train-e852a.firebaseio.com",
+    projectId: "thomas-the-train-e852a",
+    storageBucket: "",
+    messagingSenderId: "487361585735"
+};
+
+
+firebase.initializeApp(config);
+
+// create a variable to reference the database
+var database = firebase.database();
+
+//   capture the button click 
+$('#train-add').click(function () {
+    // do not want it to repeat
+    event.preventDefault();
+    // grab the values form text boxes
+    var name = $('#name-input').val().trim();
+    var destination = $('#destination-input').val().trim();
+    var time = $('#time-input').val().trim();
+    var frequency = $('#frequency-input').val();
+
+    // giving my databaes labels for the inputs
+    // using .push() to not reset one set of data but a list of 
+    database.ref().push({
+        name: name,
+        destination: destination,
+        time: time,
+        frequency: frequency
+    });
+    // firebase watcher.on('child_added')
+    // database.ref().on('')
+    // stroing the snapshot.val() in a variable for convenience
+    database.ref().on('child_added', function (snapshot) {
+        var snapshot = snapshot.val();
+        // logging the last input 
+        console.log(snapshot);
+
+        // changing the html to reflect the input
+        $('tbody').append('<tr> <td>' + snapshot.name + '</td>' +
+            '<td>' + snapshot.destination + '</td>' +
+            '<td>' + snapshot.time + '</td>' +
+            '<td>' + snapshot.frequency + '</td>' +
+            '</tr>');
+
+
+    }, function (errorObject) {
+        console.log("the read failed: " + errorObject);
+
+    });
+
+});   
